c726a463710368496bae8e31d4b7793b291b5304
montoyafranco <francomontoya9810@gmail.com>
2022-07-28 17:46:09 -0300

    Trying DTO

--- src/main/java/com/agus/hospital/dto/DTOSpeciality.java
+++ src/main/java/com/agus/hospital/dto/DTOSpeciality.java
@@ -14,6 +14,6 @@ public class DTOSpeciality {
     private Long id;
     private String name;   // max 100char  min 5
     private String physician_in_charge; // max 45 char  min 10
-    private List<Appointment> appointmentList = new ArrayList<>();
+    private List<DTOAppointment> appointmentList = new ArrayList<>();
 
 }
