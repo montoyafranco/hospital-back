c4bb23ce36b7ea1039710fde99bb70ecfe9317da
montoyafranco <francomontoya9810@gmail.com>
2022-07-28 00:33:09 -0300

    bug,fixed

--- src/main/java/com/agus/hospital/entity/Appointment.java
+++ src/main/java/com/agus/hospital/entity/Appointment.java
@@ -2,7 +2,6 @@ package com.agus.hospital.entity;
 
 import lombok.Data;
 import javax.persistence.*;
-import java.util.ArrayList;
 import java.util.List;
 
 @Entity(name = "Appointment")
@@ -12,11 +11,10 @@ public class Appointment {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-
     private String name;
     private int age;
     private Long identification_number;
-    private List<String> date_of_appointments = new ArrayList<>();
+
     private Long number_of_appointments = 1L;
     private Long fkSpecialityId;
 
